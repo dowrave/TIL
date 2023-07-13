49a2bc2adaea31533b7bf40ec32788bfecd9d504
dowrave <dowrave@gmail.com>
2023-07-14 03:06:25 +0900

    230713~14

--- NotObsidian/C#/example01.cs
+++ NotObsidian/C#/example01.cs
@@ -1,25 +1,15 @@
-// 10보다 작은 3과 5의 배수를 모두 더하면 23일 때, 1000보다 작은 자연수 중 3 또는 5의 배수를 모두 더하면 얼마일까요?
+class Computer
+{
+}
 
-using System;
-using learn;
+interface IMonitor
+{
+	void TurnOn();
+}
 
-namespace learn
-    {
-        class Program{
-        static void Main(string[] args) 
-        {
-            int result = 0;
-            int target = 10;
+interface IKeyboard {}
 
-            for (int i = 1; i < target; i ++)
-            {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                    result += i;
-                }
-            }
-            Console.WriteLine(result);
-
-        }
-    }
-    }
+class Notebook : Computer, IMonitor, IKeyboard
+{
+	public void TurnOn() {} // 추상 메서드와 달리 override를 쓰지 않는다.
+}
