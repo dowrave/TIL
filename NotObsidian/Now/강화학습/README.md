c4514b40c883ce54dfc32a20de61a1deb2d6d5b1
dowrave <dowrave@gmail.com>
2023-08-08 14:39:48 +0900

    230808

--- NotObsidian/Now/강화학습/README.md
+++
@@ -1,2 +0,0 @@
-# 원본 코드
-- [여기](https://github.com/rlcode/reinforcement-learning-kr/)에 있으며, 따라쳤음.
--- NotObsidian/Now/강화학습/environment.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-from tkinter import Button
-import time
-import numpy as np 
-from PIL import ImageTk, image
