9770c3e2cdf82e99d9b887ef7e820b578d6497ce
dowrave <dowrave@gmail.com>
2023-11-11 01:07:41 +0900

    231111

--- NotObsidian/Now/django/ch2/photo/forms.py
+++ NotObsidian/Now/django/ch2/photo/forms.py
@@ -1,6 +1,28 @@
 from photo.models import Album, Photo
 from django.forms.models import inlineformset_factory 
+from django import forms
+from django.forms.models import modelform_factory
 
 PhotoInlineFormSet = inlineformset_factory(Album, Photo, 
                                            fields = ['image', 'title', 'description'],
                                            extra = 2)
+
+
+# 17장 - 일반 폼 
+class PhotoForm(forms.Form):
+    album = forms.ModelChoiceField(queryset = Album.objects.all())
+    title = forms.CharField(max_length = 50)
+    image = forms.ImageField()
+    description = forms.CharField(label = 'Photo Description', 
+                                  widget = forms.Textarea,
+                                  requried = False)
+    upload_date = forms.DateTimeField(label = 'Upload Date')
+
+# 17장 - ModelForm
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['title', 'image', 'description']
+
+# 17장 - modelform_factory
+PhotoForm = modelform_factory(Photo, fields = '__all__')
