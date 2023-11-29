a4d2ab3cd39b4af00ab174e4ce6fa84e78912577
dowrave <dowrave@gmail.com>
2023-11-29 21:28:41 +0900

    231129 : 9장

---
+++ NotObsidian/Now/react/router-test-app/src/pages/About.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams, useNavigate } from 'react-router-dom'
+
+
+type Props = { title: string };
+
+const About = (props: Props) => {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState<number>(1);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const strPage = searchParams.get("page");
+    setPage(parseInt(strPage !== null ? strPage : "1", 10));
+  }, [searchParams]);
+
+  const goPrev = () => {
+    if (page > 1) navigate(location.pathname + "?page=" + (page - 1));
+  };
+
+  const goNext = () => {
+    navigate(location.pathname + "?page=" + (page + 1));
+  };
+
+  return (
+    <div className="card card-body">
+        <h1>About {props.title}</h1>
+        <div className="m-2">현재 페이지 : {page}
+        <button className="btn btn-secondary m-1" onClick={goPrev}>prev</button>
+        <button className="btn btn-secondary m-1" onClick={goNext}>next</button>
+        </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ NotObsidian/Now/react/router-test-app/src/pages/songs/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <>
+    <hr />
+    <h3>현재 재생 중인 곡 없음</h3>
+    </>
+  )
+}
+
+export default index
