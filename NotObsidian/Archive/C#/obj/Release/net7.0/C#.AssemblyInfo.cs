038e66f92972c23d8de67d5da49a54e0ecfea3ad
dowrave <dowrave@gmail.com>
2025-02-23 18:45:48 +0900

    250223

--- NotObsidian/Archive/C#/obj/Release/net7.0/C#.AssemblyInfo.cs
+++ NotObsidian/Archive/C#/obj/Release/net7.0/C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca315a031d19edd0b36bc3a7bdaf3f1fe2039c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
